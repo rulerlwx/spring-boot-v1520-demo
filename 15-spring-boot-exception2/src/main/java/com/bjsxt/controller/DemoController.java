9e3f5f9d2131bee0a1c6f968ad23c5b6686339d7
black_wolf <leafsing20309@163.com>
2019-05-09 10:02:02 +0800

    Initial commit

---
+++ 15-spring-boot-exception2/src/main/java/com/bjsxt/controller/DemoController.java
@@ -0,0 +1,57 @@
+package com.bjsxt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * SpringBoot处理异常方式一：自定义错误页面
+ *
+ *
+ */
+@Controller
+public class DemoController {
+	
+	@RequestMapping("/show")
+	public String showInfo(){
+		String str = null;
+		str.length();
+		return "index";
+	}
+	
+	@RequestMapping("/show2")
+	public String showInfo2(){
+		int a = 10/0;
+		return "index";
+	}
+	
+	/**
+	 * java.lang.ArithmeticException
+	 * 该方法需要返回一个ModelAndView：目的是可以让我们封装异常信息以及视图的指定
+	 * 参数Exception e:会将产生异常对象注入到方法中
+	 */
+	@ExceptionHandler(value={ArithmeticException.class})
+	public ModelAndView arithmeticExceptionHandler(Exception e){
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("error", e.toString());
+		mv.setViewName("error1");
+		return mv;
+	}
+	
+	/**
+	 * java.lang.NullPointerException
+	 * 该方法需要返回一个ModelAndView：目的是可以让我们封装异常信息以及视图的指定
+	 * 参数Exception e:会将产生异常对象注入到方法中
+	 */
+	@ExceptionHandler(value={NullPointerException.class})
+	public ModelAndView nullPointerExceptionHandler(Exception e){
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("error", e.toString());
+		mv.setViewName("error2");
+		return mv;
+	}
+	
+	
+	
+}
---
+++ 19-spring-boot-test/src/test/java/com/bjsxt/test/UserServiceTest.java
@@ -0,0 +1,33 @@
+package com.bjsxt.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.bjsxt.App;
+import com.bjsxt.service.UserServiceImpl;
+
+/**
+ * SpringBoot测试类
+ *@RunWith:启动器 
+ *SpringJUnit4ClassRunner.class：让junit与spring环境进行整合
+ *
+ *@SpringBootTest(classes={App.class})有两层含义：
+ * 1,当前类为springBoot的测试类
+ * 2,加载SpringBoot启动类。启动springBoot
+ *
+ *junit与spring整合 @Contextconfiguartion("classpath:applicationContext.xml")
+ */
+@RunWith(SpringJUnit4ClassRunner.class) 
+@SpringBootTest(classes={App.class})
+public class UserServiceTest {
+
+	@Autowired
+	private UserServiceImpl userServiceImpl;
+	
+	@Test
+	public void testAddUser(){
+		this.userServiceImpl.addUser();
+	}
+}
